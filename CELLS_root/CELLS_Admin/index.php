e1eb14934774333ba9eaedea6d69190be665d5e4
dareyouuu <raihanakbar1717@gmail.com>
2024-10-30 09:57:05 +0700

    tes

--- CELLS_root/CELLS_Admin/index.php
+++ CELLS_root/CELLS_Admin/index.php
@@ -19,7 +19,7 @@ $sql = "SELECT * FROM about_us ORDER BY Foto DESC";
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Dashboard - SB Admin</title>
+        <title>Dashboard</title>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
         <link href="css/styles.css" rel="stylesheet" />
