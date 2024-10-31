842bb57ebd599522da87c80da1910c30d1376605
Ilyasayunus <yunusilyasa@gmail.com>
2024-10-31 14:09:15 +0700

    change route membership to page construction

--- CELLS_root/page_construction.php
+++ CELLS_root/page_construction.php
@@ -32,82 +32,38 @@
 </head>
 
 <body>
-    <!-- Spinner Start -->
-    <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
-        <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
-            <span class="sr-only">Loading...</span>
-        </div>
-    </div>
-    <!-- Spinner End -->
-
-
-    <!-- Navbar Start -->
-    <nav class="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
-		
-		<a href="index.html"><img src="img/Logo_CELLS_UPI.png" alt="CELLS UPI Logo" width="200" height="106" class="img-fluid d-flex align-items-center px-lg-5"/></a>
-		
-<!--
-        <a href="index.html" class="navbar-brand d-flex align-items-center px-4 px-lg-5 container-fluid">
-        	
-		</a>
--->
-		
-        <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-		
-      <div class="collapse navbar-collapse" id="navbarCollapse">
-            <div class="navbar-nav ms-auto p-4 p-lg-0">
-                <a href="index.php" class="nav-item nav-link">Home</a>
-                <a href="about_us.php" class="nav-item nav-link">About Us</a>
-                <a href="projects.php" class="nav-item nav-link">Projects</a>
-				<a href="publication.html" class="nav-item nav-link">Publication</a>
-				<a href="lesson_studies.php" class="nav-item nav-link">Open Lesson Studies</a>
-				<a href="vacancies.html" class="nav-item nav-link">Vacancies</a>
-<!--	
-				DROPDOWN MENU TEMPLATE
-                <div class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle active" data-bs-toggle="dropdown">Pages</a>
-                    <div class="dropdown-menu fade-down m-0">
-                        <a href="team.html" class="dropdown-item active">Our Team</a>
-                        <a href="testimonial.html" class="dropdown-item">Testimonial</a>
-                        <a href="404.html" class="dropdown-item">404 Page</a>
-                    </div>
-                </div>
-				
--->
-            </div>
-<!--
-			JOIN NOW
-            <a href="" class="btn btn-primary py-4 px-lg-5 d-none d-lg-block">Join Now<i class="fa fa-arrow-right ms-3"></i></a>
--->
-        </div>
-    </nav>
-    <!-- Navbar End -->
+    <?php include "header.php" ?>
 
 
     <!-- Header Start -->
-    <div class="container-fluid bg-primary py-5 page-header" style="width: 100vw; height: 100vh;">
+    <div class="container-fluid bg-primary py-5 mb-5 page-header">
         <div class="container py-5">
             <div class="row justify-content-center">
                 <div class="col-lg-10 text-center">
-                    <h1 class="display-3 text-white animated slideInDown">Open Lesson Studies</h1>
-                    <nav aria-label="breadcrumb">
-                        <ol class="breadcrumb justify-content-center">
-                            <li class="breadcrumb-item"><a class="text-white" href="index.php">Home</a></li>
-                            <li class="breadcrumb-item"><a class="text-white" href="#">Pages</a></li>
-                            <!-- <li class="breadcrumb-item"><a class="text-white" href="../CELLS_root/membership/login.php">Membership</a></li></li> -->
-                            <li class="breadcrumb-item"><a class="text-white" href="../CELLS_root/page_construction.php">Membership</a></li></li>
-                        </ol>
-                    </nav>
+                    <h1 class="display-3 text-white animated slideInDown">Membership</h1>
                 </div>
             </div>
         </div>
     </div>
-    
     <!-- Header End -->
 
-        
+
+    <!-- 404 Start -->
+    <div class="container-xxl py-5 wow fadeInUp" data-wow-delay="0.1s">
+        <div class="container text-center">
+            <div class="row justify-content-center">
+                <div class="col-lg-6">
+                    <i class="bi bi-exclamation-triangle display-1 text-primary"></i>
+					<!--<h1 class="display-1">Oops, We're Sorry</h1>-->
+                    <h1 class="mb-4">This Page is Still Under Construction</h1>
+                    <p class="mb-4">We’re sorry, the page you are looking for is still under construction! Maybe you can go to our home page for now.</p>
+                    <a class="btn btn-primary rounded-pill py-3 px-5" href="index.php">Go Back To Home</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- 404 End -->
+
 
     <!-- Footer Start -->
     <div class="container-fluid bg-dark text-light footer pt-5 mt-5 wow fadeIn" data-wow-delay="0.1s">
@@ -115,20 +71,20 @@
             <div class="row g-5">
                 <div class="col-lg-3 col-md-6">
                     <h4 class="text-white mb-3">Quick Link</h4>
-                    <a class="btn btn-link" href="about_us.php">About Us</a>
-                    <a class="btn btn-link" href="projects.php">Projects</a>
+                    <a class="btn btn-link" href="about_us.html">About Us</a>
+                    <a class="btn btn-link" href="projects.html">Projects</a>
                     <a class="btn btn-link" href="publication.html">Publication</a>
-                    <a class="btn btn-link" href="partner_and_network.html">Open Lesson Studies</a>
+                    <a class="btn btn-link" href="partner_and_network.html">Partner and Network</a>
                     <a class="btn btn-link" href="vacancies.html">Vacancies</a>
                 </div>
-				
+
                 <div class="col-lg-3 col-md-6">
                     <h4 class="text-white mb-3"><a href="contact.html" class="contact-href">Contact</a></h4>
                     <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>Jl. Dr. Setiabudhi No. 229, Bandung, West Java, Indonesia</p>
 <!--
-					
+
                     <p class="mb-2"><i class="fa fa-phone-alt me-3"></i><a href="tel:+4733378901">+012 345 67890</p>
-						
+
 -->
                     <p class="mb-2"><i class="fa fa-envelope me-3"></i><a href="mailto:admin_cells@upi.edu" target="_blank" class="custom-email">admin_cells@upi.edu</a></p>
                     <div class="d-flex pt-2">
@@ -139,7 +95,7 @@
                         <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-linkedin-in"></i></a>
                     </div>
                 </div>
-				
+
                 <div class="col-lg-3 col-md-6">
                     <h4 class="text-white mb-3">Gallery</h4>
                     <div class="row g-2 pt-2">
@@ -163,7 +119,7 @@
                         </div>
                     </div>
                 </div>
-				
+
 
 <!--
                 <div class="col-lg-3 col-md-6">
@@ -192,10 +148,10 @@ hs.src = ('//s10.histats.com/js15_as.js');
 <noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4891304&101" alt="" border="0"></a></noscript>
 <!-- Histats.com  END  -->
 				</div>
-				
+
             </div>
         </div>
-		
+
         <div class="container">
             <div class="copyright">
                 <div class="row">
@@ -205,7 +161,7 @@ hs.src = ('//s10.histats.com/js15_as.js');
                         <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
                         Designed By <a class="border-bottom" href="https://htmlcodex.com">HTML Codex</a>
                     </div>
-	
+
 <!--
                     <div class="col-md-6 text-center text-md-end">
                         <div class="footer-menu">
@@ -216,7 +172,7 @@ hs.src = ('//s10.histats.com/js15_as.js');
                         </div>
                     </div>
 -->
-					
+
                 </div>
             </div>
         </div>
